b8c9835c302a6707cf3530efe04b405dccd05f3a
SimpleMoves <simplemoves.soft@gmail.com>
2024-12-08 16:15:54 +0400

    v0.0.39 Another round of Caption handling

--- src/Wontopia.tsx
+++ src/Wontopia.tsx
@@ -3,10 +3,10 @@ import { Col, Row } from "antd";
 
 export const Wontopia = () => {
   return (
-    <Row justify="space-between" wrap={false} style={{backgroundColor: 'red', minWidth: '100%'}}>
+    <Row justify="space-between" wrap={false} style={{backgroundColor: 'red', minWidth: '100%', alignItems: 'baseline'}}>
     {/* <Flex vertical={false} gap={'middle'} justify={'space-between'} className={'main-caption'}> */}
-      <Col>WONTOPIA</Col>
-      <Col>v0.1.0</Col>
+      <Col className='main-title1'><div className='fg'>WONTOPIA</div></Col>
+      <Col className='version'>v0.1.0</Col>
       {/* <div className='main-title1'>
         <div className='fg'>WONTOPIA</div>
       </div>
