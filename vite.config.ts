a020b33c7bf6a2f2aa1f5c3d4cdc68c7a3d555e2
SimpleMoves <simplemoves.soft@gmail.com>
2025-04-03 21:31:17 +0400

    v0.10.0 pages try

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import tsconfigPaths from 'vite-tsconfig-paths';
-// import fs from "fs";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-    // server: {
-    //     https: {
-    //         key: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/private.key.pem", "utf-8"),
-    //         cert: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/domain.cert.pem", "utf-8"),
-    //     },
-    //     host: "internal.wontopia.win", // Ensure it runs on localhost
-    //     port: 5137, // Change if needed
-    // },
+    server: {
+        https: {
+            key: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/private.key.pem", "utf-8"),
+            cert: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/domain.cert.pem", "utf-8"),
+        },
+        host: "internal.wontopia.win", // Ensure it runs on localhost
+        port: 5137, // Change if needed
+    },
     plugins: [
         react(),
         tsconfigPaths(),
