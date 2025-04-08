c905a8ab94d00363ccb03682a00151628f7ffce4
SimpleMoves <simplemoves.soft@gmail.com>
2025-04-08 10:40:21 +0400

    v0.13.0 configure domain name

--- vite.config.ts
+++ vite.config.ts
@@ -5,25 +5,31 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // import fs from "fs";
 
 // https://vite.dev/config/
-export default defineConfig({
-    // server: {
-    //     https: {
-    //         key: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/private.key.pem", "utf-8"),
-    //         cert: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/domain.cert.pem", "utf-8"),
-    //     },
-    //     host: "internal.wontopia.win", // Ensure it runs on localhost
-    //     port: 5137, // Change if needed
-    // },
-    plugins: [
-        react(),
-        tsconfigPaths(),
-        nodePolyfills({
-            globals: {
-                Buffer: true,
-            },
-        }) ],
-    build: {
-        outDir: './dist',
-    },
-    base: '/wontopia/',
+export default defineConfig(({ mode }) => {
+    const isDev = (mode === 'development');
+    if (isDev) console.log("Development environment")
+
+    return {
+        // server: {
+        //     https: {
+        //         key: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/private.key.pem", "utf-8"),
+        //         cert: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/domain.cert.pem", "utf-8"),
+        //     },
+        //     host: "internal.wontopia.win", // Ensure it runs on localhost
+        //     port: 5137, // Change if needed
+        // },
+        plugins: [
+            react(),
+            tsconfigPaths(),
+            nodePolyfills({
+                globals: {
+                    Buffer: true,
+                },
+            }) ],
+        build: {
+            outDir: './dist',
+        },
+        // base: '/wontopia/',
+        base: '/',
+    }
 })
