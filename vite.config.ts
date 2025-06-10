64cfe9e2850308101411612ceb98e6113bbc13a9
SimpleMoves <simplemoves.soft@gmail.com>
2025-06-10 17:04:15 +0400

    Fixing vite.config.ts for production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import tsconfigPaths from 'vite-tsconfig-paths';
-import * as fs from "node:fs";
-// import fs from "fs";
+import tsconfigPaths from 'vite-tsconfig-paths'
+import * as fs from 'node:fs'
 
-// https://vite.dev/config/
+
 export default defineConfig(({ mode }) => {
-    const isDev = (mode === 'development');
-    if (isDev) console.log("Development environment")
+    const isDev = mode === 'development'
 
-    return {
-        server: {
-            https: {
-                key: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/private.key.pem", "utf-8"),
-                cert: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/domain.cert.pem", "utf-8"),
-            },
-            host: "internal.wontopia.win", // Ensure it runs on localhost
-            port: 5137, // Change if needed
-        },
+    const basePath = isDev ? '/wontopia/' : '/'
+
+    const commonConfig = {
         plugins: [
             react(),
             tsconfigPaths(),
@@ -26,11 +18,27 @@ export default defineConfig(({ mode }) => {
                 globals: {
                     Buffer: true,
                 },
-            }) ],
+            }),
+        ],
         build: {
             outDir: './dist',
         },
-        base: '/wontopia/',
-        // base: '/',
+        base: basePath,
     }
+
+    if (isDev) {
+        return {
+            ...commonConfig,
+            server: {
+                https: {
+                    key: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/private.key.pem", "utf-8"),
+                    cert: fs.readFileSync("/Users/fan/Documents/Wontopia/certs/domain.cert.pem", "utf-8"),
+                },
+                host: "internal.wontopia.win",
+                port: 5137,
+            },
+        }
+    }
+
+    return commonConfig
 })
